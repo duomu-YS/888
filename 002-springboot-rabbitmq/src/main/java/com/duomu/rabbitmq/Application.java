a6dc645b21cf4a43e1010bde3872adc10b81ec2e
duomu <1090450431@qq.com>
2020-12-19 20:38:30 +0800

    111

--- 002-springboot-rabbitmq/src/main/java/com/duomu/rabbitmq/Application.java
+++ 002-springboot-rabbitmq/src/main/java/com/duomu/rabbitmq/Application.java
@@ -12,7 +12,7 @@ public class Application {
 
 		ApplicationContext ac = SpringApplication.run(Application.class, args);
 		SendServer sendServer = (SendServer) ac.getBean("sendService");
-		sendServer.sendTopicMessage("准备睡觉Q！11！！！");
+		sendServer.sendTopicMessage("准备睡觉Q！11！1！！");
 		sendServer.sendTopicMessage("准备睡觉Q！！！！");
 	}
 
